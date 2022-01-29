77d7c236e17a5238820fc435009348254223d68c
FrancoMusolino <francomusolino55@gmail.com>
2022-01-29 15:50:37 -0300

    Add element to array & more styles and functions

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const ventanaModal = document.querySelector('.ventana-modal');
 const selectCategories = document.getElementById('categories');
 const addImg = document.getElementById('add-button');
 const deleteImg = document.getElementById('delete-image');
-const deleteButton = document.getElementById('delete-container');
+const trashIcon = document.getElementById('trash-icon');
+const deleteContainer = document.getElementById('delete-container');
+const deleteButton = document.getElementById('delete-button');
+const cancelButton = document.getElementById('cancel-button');
 
 let images = [];
 
@@ -47,11 +50,10 @@ closeButton.addEventListener('click', () => {
     selectCategories.value = "";
 });
 
-const introduceNewImg = () => {
-    let newItem = images.pop()
-    templateImg.querySelector('img').setAttribute('src', newItem.src);
-    templateImg.querySelector('img').setAttribute('alt', newItem.category);
-    templateImg.querySelector('div').setAttribute('id', newItem.id)
+const introduceNewImg = newImage => {
+    templateImg.querySelector('img').setAttribute('src', newImage.src);
+    templateImg.querySelector('img').setAttribute('alt', newImage.category);
+    templateImg.querySelector('div').setAttribute('id', newImage.id)
 
 
     const clone = templateImg.cloneNode(true);
@@ -65,59 +67,102 @@ const addImage = e => {
     } else {
         let category = selectCategories.value;
 
-        /* Agrega una imagen al mismo array, por eso nunca suma más de 1 */
-
-        images.push({
+        let newImage = {
             src: `https://placeimg.com/640/480/${category}`,
             alt: category,
             id: Date.now()
-        })
+        }
+
+        introduceNewImg(newImage);
+        let newImages = [...images, newImage];
+        images = newImages;
         ventanaModal.classList.remove('visible');
         selectCategories.value = "";
-        localStorage.setItem('images', JSON.stringify(images));
-        introduceNewImg();
+        localStorage.setItem('images', JSON.stringify(newImages));
     }
 }
 
 addImg.addEventListener('click', e => addImage(e));
 
 const openCheckbox = () => {
-    const imagesInDom = document.querySelectorAll('.template-img__container');
-    imagesInDom.forEach(item => {
-        const itemCheckbox = item.querySelector('input');
-        if (item.getAttribute('id') > 10) {
-            itemCheckbox.classList.add('visible-checkbox');
-        }
+    trashIcon.classList.toggle('navbar__icon--animation');
+    if (images.length === 0) {
+        alert("No hay imágenes para eliminar");
+    } else {
+        deleteContainer.classList.toggle('delete-container--visible');
+        const imagesInDom = document.querySelectorAll('.template-img__container');
+        imagesInDom.forEach(item => {
+            const itemCheckbox = item.querySelector('input');
+            itemCheckbox.classList.toggle('visible-checkbox');
 
-        item.addEventListener('click', () => {
-            if (!itemCheckbox.getAttribute('checked')) {
-                itemCheckbox.setAttribute('checked', true);
-            } else {
-                itemCheckbox.removeAttribute('checked');
-            }
+            item.addEventListener('click', () => {
+                if (!itemCheckbox.getAttribute('checked')) {
+                    itemCheckbox.setAttribute('checked', true);
+                } else {
+                    itemCheckbox.removeAttribute('checked');
+                }
+            });
         });
-    });
-
-    deleteButton.classList.add('delete-container--visible');
+    }
 }
 
 deleteImg.addEventListener('click', openCheckbox);
 
+const deleteItemOfArray = id => {
+    console.log(id, images)
+    let newImages = images.filter(el => {
+        el.id !== id
+        console.log(el.id)
+        console.log(id)
+    }
+    );
+    console.log(newImages);
+    // localStorage.setItem('images', JSON.stringify(newImages));
+}
+
 const deleteImage = () => {
     const imagesInDom = document.querySelectorAll('.template-img__container');
-    let auth = confirm('¿Seguro desea eliminar las imágenes?');
+    let contador = 0;
+    imagesInDom.forEach(item => {
+        const itemCheckbox = item.querySelector('input');
+        if (itemCheckbox.checked) {
+            contador++;
+        }
+    });
+
+    if (contador === 0) {
+        alert("No hay imágenes seleccionadas para eliminar");
+        return;
+    }
+
+    let auth = contador === 1 ? confirm('¿Desea eliminar la imagen?') :
+        confirm(`¿Desea eliminar las ${contador} imágenes?`);
 
     if (auth) {
         imagesInDom.forEach(item => {
             const itemCheckbox = item.querySelector('input');
             if (itemCheckbox.checked) {
                 let itemId = item.getAttribute('id');
+                deleteItemOfArray(itemId)
                 container.removeChild(item);
-                let newImages = images.filter(image => image.id !== itemId);
-                localStorage.setItem('images', JSON.stringify(newImages));
             }
         })
     }
 }
 
 deleteButton.addEventListener('click', deleteImage);
+
+const cancelImgDelete = () => {
+    deleteContainer.classList.remove('delete-container--visible');
+    trashIcon.classList.remove('navbar__icon--animation');
+    const imagesInDom = document.querySelectorAll('.template-img__container');
+    imagesInDom.forEach(item => {
+        const itemCheckbox = item.querySelector('input');
+        if (itemCheckbox.getAttribute('checked')) {
+            itemCheckbox.removeAttribute('checked');
+        }
+        itemCheckbox.classList.remove('visible-checkbox');
+    })
+}
+
+cancelButton.addEventListener('click', cancelImgDelete);
